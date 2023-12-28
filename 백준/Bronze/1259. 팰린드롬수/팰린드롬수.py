806d1855f576fde76823a127a9e748bd0d188d4a
coooow0 <105861622+coooow0@users.noreply.github.com>
2023-12-28 23:57:05 +0900

    [Bronze I] Title: 팰린드롬수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,8 +1,14 @@
-while True:
-    n = input()
+import sys
+# strip은 양쪽 공백을 삭제함 
+
+while True: 
+    n = sys.stdin.readline().strip()
     if n == '0':
         break
-    if n == n[::-1]:
+
+    n = list(n)
+
+    if n==list(reversed(n)):
         print('yes')
     else:
         print('no')
