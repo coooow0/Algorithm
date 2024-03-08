1ed5fa4feb12f032eb5f23fe0f08e465821ca83e
coooow0 <105861622+coooow0@users.noreply.github.com>
2024-03-09 02:03:02 +0900

    [Bronze II] Title: 블랙잭, Time: 88 ms, Memory: 37492 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,18 +1,14 @@
-from itertools import combinations
-
 n, m = map(int, input().split())
 #n은 카드의 개수, m은 넘지 말아야 하는 수
 arr = list(map(int, input().split()))
 result = []
 
-for i in combinations(arr, 3):
-    if sum(i) > m:
-        continue
-    else:
-        result.append(sum(i))
-
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            sum = arr[i] + arr[j] + arr[k]
+            if sum > m:
+                continue
+            else:
+                result.append(sum)
 print(max(result))
-
-# combinations(a, n)
-# a에 반복문이나 리스트 등의 반복 가능한 것을 넣음
-# n에 중복을 허용하지 않고 뽑을 개수를 넣음
