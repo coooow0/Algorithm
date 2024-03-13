67b0dbe51791768e87f6c9c29dec4316fd370738
coooow0 <105861622+coooow0@users.noreply.github.com>
2024-03-13 23:10:22 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,10 +1,12 @@
-n, m = map(int, input().split())
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
 arr = [0] * (n+1)
 for i in range(1, n+1):
     arr[i] = i
 
 for _ in range(m):
-    a, b = map(int, input().split())
+    a, b = map(int, sys.stdin.readline().split())
     temp = arr[a]
     arr[a] = arr[b]
     arr[b] = temp
