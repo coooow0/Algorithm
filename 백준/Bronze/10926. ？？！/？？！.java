13c64a15104cda1f8acd5dffa2529e2ce8b26738
coooow0 <105861622+coooow0@users.noreply.github.com>
2024-04-08 23:20:22 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.nio.Buffer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(reader);
+        String a = br.readLine();
+        System.out.println(a + "??!");
+    }
+}
