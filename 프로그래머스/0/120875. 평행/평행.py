d60403425eba028385b7a44625c0c5d711bf6e45
coooow0 <105861622+coooow0@users.noreply.github.com>
2024-06-13 01:50:48 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -0,0 +1,6 @@
+def solution(dots):
+    dots.sort(key=lambda x:x[0])
+    if  (dots[1][1] - dots[0][1]) / (dots[1][0] - dots[0][0]) == (dots[3][1] - dots[2][1]) / (dots[3][0] - dots[2][0]):
+        return 1
+    else:
+        return 0
