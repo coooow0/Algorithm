123016a8ec8f58b3e0ed03209523f10c8405fb9e
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2024-09-27 00:20:30 +0900

    [Silver I] Title: 카드 구매하기, Time: 188 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -1,4 +1,6 @@
 # 카드 구매하기
+import sys
+input = sys.stdin.readline
 
 n = int(input())
 p = [0] + list(map(int, input().split()))
@@ -9,4 +11,4 @@ for i in range(1, n+1): # i장의 카드를 뽑을 때 최대값
     for k in range(1, i+1): # 카드팩 P[k]의 값..
         dp[i] = max(dp[i], dp[i-k] + p[k])
         # 최대값을 더하다보면 전의 값에서 + 하는 것을 볼 수 있음. 
-print(max(dp))
+print(dp[-1])
