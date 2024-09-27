d54510b9e9571fe719609b2e03c39728cd4862e8
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2024-09-28 05:03:59 +0900

    [Silver II] Title: 정수 삼각형, Time: 108 ms, Memory: 36532 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -4,20 +4,17 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-dp = [[0] * n for _ in range(n+1)]
+dp = [[0] * n for _ in range(n)]
 
 for i in range(n):
     dp[i]=(list(map(int, input().split())))
-    for j in range(n-i):
-        dp[i].append(0)
-        
+    dp[i].append(0)
 if n > 1:    
     dp[1][0] += dp[0][0]
     dp[1][1] += dp[0][0]
         
     for i in range(2, n):
         dp[i][0] += dp[i-1][0]
-        for j in range(1, n+1):
+        for j in range(1, i+1):
             dp[i][j] += max(dp[i-1][j-1], dp[i-1][j])
 print(max(dp[n-1])) 
-    
