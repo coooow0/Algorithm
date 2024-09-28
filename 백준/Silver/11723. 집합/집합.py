f535995979c6eb73b120e2f866d5a6e4c524cc50
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2024-09-28 18:38:48 +0900

    [Silver V] Title: 집합, Time: 3892 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -5,15 +5,17 @@ input = sys.stdin.readline
 n = int(input())
 
 s = set()
-
 for i in range(n):
     arr = input().rstrip().split()
+
     if arr[0] == 'add':
         s.add(int(arr[1]))
     elif arr[0] == 'check':
-        print(1 if int(arr[1]) in s else 0)
+        if int(arr[1]) in s:
+            print(1)
+        else: print(0)
     elif arr[0] == 'remove':
-        s.discard(int(arr[1]))  # remove 대신 discard를 쓰면, 존재하지 않아도 에러가 발생하지 않음
+        s.discard(int(arr[1])) # 삭제하려는 것이 없어도 오류가 나지 않음. remove는 오류가 나서 if문을 통해 확인해야함
     elif arr[0] == 'toggle':
         x = int(arr[1])
         if x in s:
@@ -21,6 +23,6 @@ for i in range(n):
         else:
             s.add(x)
     elif arr[0] == 'all':
-        s = set([i for i in range(1, 21)])
-    elif arr[0] == 'empty':
-        s.clear()
+        s = {i for i in range(1, 21)}
+    else:
+        s = set()
