5a7528be46e8a1d9a77ce1bd43fb1e02aca5d5f2
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2024-10-01 01:38:20 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 52 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,4 +1,5 @@
-# # 요세푸스 문제
+# 요세푸스 문제
+
 from collections import deque
 
 n, k = map(int, input().split())
@@ -7,9 +8,8 @@ d = deque([i for i in range(1, n+1)])
 answer = []
 
 while d:
-    for _ in range(k-1):
-        d.append(d.popleft())
+    d.rotate(-(k-1))
+    # rotate(n)
+    # n이 양수면 n칸 만큼 오른쪽으로 이동하고, 음수면 맨 뒤에서 부터 시작함..
     answer.append(d.popleft())
-
 print(str(answer).replace('[', '<').replace(']', '>'))
-
