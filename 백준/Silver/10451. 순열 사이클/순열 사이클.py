eb2a4bb87749e7da6417bc171cc8404f42198c27
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2024-10-04 01:06:12 +0900

    [Silver III] Title: 순열 사이클, Time: 520 ms, Memory: 31228 KB -BaekjoonHub

---
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.py
@@ -0,0 +1,30 @@
+# 순열 사이클
+
+def dfs(v):
+    visited[v] = True
+
+    for next in graph[v]:
+        if not visited[next]:
+            dfs(next)
+
+tc = int(input())
+
+for _ in range(tc):
+    n = int(input())
+    arr = [0]
+    arr += list(map(int, input().split()))
+
+    graph = [[] for _ in range(n + 1)]
+    visited = [False] * (n + 1)
+    
+    for i in range(1, n+1):
+        graph[i].append(arr[i])
+        
+    answer = 0
+    
+    for i in range(1, n+1):
+        if not visited[i]:
+            dfs(i)
+            answer += 1
+        
+    print(answer)
