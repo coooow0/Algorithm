0f01d018576e4c4f03059a356d63a913ef0086b1
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2024-10-03 13:49:45 +0900

    [Silver II] Title: DFS와 BFS, Time: 112 ms, Memory: 38464 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,38 +1,41 @@
-from collections import deque
+# DFS와 BFS
 import sys
+def DFS(idx):
+    global visited
+    visited[idx] = True # 방문 처리
+    print(idx, end=' ')
+    for next in range(1, n+1):
+        if not visited[next] and graph[next][idx]:
+            # 방문을 하지 않았고, 그래프에 위치가 True일 경우
+            DFS(next)
+
+def BFS():
+     global q, visited
+     while q:
+         cur = q.pop(0)
+         print(cur, end = ' ')
+         for next in range(1, n+1):
+             if not visited[next] and graph[cur][next]:
+                 visited[next] = True
+                 q.append(next)
+                 
+
 input = sys.stdin.readline
-
 n, m, v = map(int, input().split())
+graph = [[False] * (n + 1) for _ in range(n+1)]
+visited = [False] * (n+1)
 
-graph = [[0] * (n+1) for _ in range(n+1)]
-
-visited1 = [0] * (n+1)
-visited2 = visited1.copy()
-
-for i in range(m):
+for _ in range(m): # 간선의 개수
     a, b = map(int, input().split())
-    graph[a][b] = graph[b][a] = 1
-    # 이어진 간선을 만들기 위해 a, b 위치를 바꾸어서도 1로 만들어줌
-
-def DFS(v):
-    visited1[v] = 1
-    print(v, end=' ')
-    for i in range(n+1):
-        if graph[v][i] == 1 and visited1[i] == 0:
-            DFS(i)
-            # 그래프에 1이 칠해져 있고, 방문하지 않은 노드일 때
-
-def BFS(v):
-    visited2[v] = 1
-    queue = deque([v])
-    while queue:
-        v = queue.popleft()
-        print(v, end=' ')
-        for i in range(n+1):
-            if graph[v][i] == 1 and visited2[i] == 0:
-                queue.append(i)
-                visited2[i] = 1
-
+    graph[a][b], graph[b][a] = True, True
+    # 정점을 있다고 표시함
+    
 DFS(v)
 print()
-BFS(v)
+
+visited = [False] * (n+1)
+q = [v] 
+visited[v] = True # 이미 방문 처리를 함
+BFS()
+
+
