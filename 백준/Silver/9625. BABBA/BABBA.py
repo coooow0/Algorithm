ee19c9bdecf60cd7e0d5bcca361662475d7b8b27
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2024-10-22 20:36:55 +0900

    [Silver V] Title: BABBA, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9625. BABBA/BABBA.py
+++ 백준/Silver/9625. BABBA/BABBA.py
@@ -1,10 +1,6 @@
 k = int(input())
 
-dp = [[0, 0] for _ in range(k + 1)]
-dp[1] = [0, 1]
-
-if k >=2:
-    dp[2] = [1, 1]
-    for i in range(3, k + 1):
-        dp[i] = [dp[i-2][0]+ dp[i-1][0], dp[i-2][1] + dp[i-1][1]]
-print(str(dp[k]).replace('[', '').replace(']', '').replace(',', ''))
+a, b = 0, 1
+for _ in range(1, k):
+    a, b = b, a+b
+print(a, b)
