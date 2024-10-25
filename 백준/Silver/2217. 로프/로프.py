2a3a68fb92646bc75633b61d13c2cda17147a959
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2024-10-25 22:47:50 +0900

    [Silver IV] Title: 로프, Time: 7916 ms, Memory: 223532 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+arr = []
+
+for _ in range(n):
+    arr.append(int(input().strip()))
+arr.sort()
+result = 0
+for i in range(n):
+    result = max(result, arr[i] * len(arr[i:]))
+    
+print(result)
