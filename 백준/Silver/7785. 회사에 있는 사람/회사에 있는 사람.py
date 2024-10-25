efca913ed3e58d27b7308e7685d6da9aeaa79b7c
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2024-10-25 18:31:40 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 236 ms, Memory: 48332 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,17 +1,14 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
+n = int(input().strip())
 log = {}
-for _ in range(n):
+for i in range(n):
     name, data = map(str, input().split())
-    log[name] = data
+    if data == 'enter':
+        log[name] = data
+    else:
+        del log[name]
 
-result = []
+log = sorted(log, reverse=True)
 for i in log:
-    if log[i] == 'enter':
-        result.append(i)
-    # 회사에 있는 사람들만 배열에 넣어줌
-
-result.sort(reverse=True)
-# 내림차순으로 정렬
-
-for i in result:
     print(i)
