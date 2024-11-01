8556ac3f3687f0e2a9b9be9d7e745d0e04bc302e
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2024-11-01 16:50:38 +0900

    [Gold IV] Title: 공유기 설치, Time: 5248 ms, Memory: 38848 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,33 +1,25 @@
-# 공유기 설치
-
 n, c = map(int, input().split())
-# 집의 개수, 공유기의 수
-house = []
+arr = []
 for _ in range(n):
-    house.append(int(input()))
-# 집의 좌표
-house.sort()
+    arr.append(int(input()))
+arr.sort()
 
-left, right = 1, house[-1]-house[0]
 result = 0
 
-while right >= left:
+left, right = 1, arr[-1]-arr[0]
+while left <= right:
     mid = (left + right) // 2
-    # 중앙 수
     cnt = 1
-    # mid 거리만큼 거리두었을 때 공유기의 개수를 셈
-    stand = house[0]
+    standard = arr[0]
     
-    for i in range(1, n):
-        if house[i] >= stand + mid:
+    for i in range(n):
+        if arr[i] - standard >= mid:
             cnt += 1
-            stand = house[i]
-    # print(cnt)
+            standard = arr[i]
+    
     if cnt >= c:
-        # cnt가 더 크면? mid가 너무 작다는 것. 하나 더 더함
         result = mid
         left = mid + 1
-    
     else:
         right = mid - 1
         
