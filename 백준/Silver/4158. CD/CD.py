054c9272ef7c487fd7c09c2f29ee43e318226d4e
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2025-04-09 14:57:39 +0900

    [Silver V] Title: CD, Time: 1904 ms, Memory: 218164 KB -BaekjoonHub

---
+++ 백준/Silver/4158. CD/CD.py
@@ -0,0 +1,31 @@
+# CD
+while True:  
+    n, m = map(int, input().split())
+    if n == 0 and m == 0:
+        break
+    
+    a = []
+    for _ in range(n):
+        a.append(int(input()))
+        
+    b = []
+    for _ in range(m):
+        b.append(int(input()))
+    
+    first = 0 # a를 탐색할 변수
+    second = 0 # b를 탐색할 변수
+
+    cnt = 0
+
+    while first < n and second < m:
+        # 해당 조건을 만족할때만 동작
+        cd_1, cd_2 = a[first], b[second]
+        if cd_1 < cd_2:
+            first += 1
+        elif cd_1 > cd_2:
+            second += 1
+        else:
+            cnt += 1
+            first += 1
+            second += 1
+    print(cnt)
