796af47017674ccd0b322986084293a636d766c0
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2025-06-30 13:59:10 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,7 +1,7 @@
-import sys
-stu = [0] * 31 # 30 + 1
-for i in range(28):
-    stu[int(sys.stdin.readline())] = 1
-for i in range(1, 31):
-    if stu[i] == 0:
-        print(i)
+arr = [i for i in range(1, 31)]
+
+for _ in range(28):
+    arr.remove(int(input()))
+
+for i in arr:
+    print(i)
