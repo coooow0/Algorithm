33d6ad8e0fa5f6a7b826eb85825e8189fd3bc016
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2025-07-01 13:59:09 +0900

    [Silver IV] Title: 스택, Time: 408 ms, Memory: 111928 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,30 +1,30 @@
-import sys
-n = int(sys.stdin.readline().strip())
+# 스택
+n = int(input())
 stack = []
 
-for i in range(n):
-    inner = sys.stdin.readline().strip().split()
-    if inner[0] == 'push':
-        stack.append(int(inner[1]))
-    else:
-        if inner[0] == 'pop':
-            if stack: #비어있지 않으면
-                print(stack[-1])
-                stack.pop()
-            else: #비어있으면
-                print(-1)
-
-        elif inner[0] == 'size':
-            print(len(stack))
-
-        elif inner[0] == 'empty':
-            if stack: #비어있지 않으면 
-                print(0)
-            else:
-                print(1)
-        else: #top
-            if stack:  # stack이 비어있지 않은 경우
-                print(stack[-1])
-            else:
-                print(-1)
-
+for _ in range(n):
+    arr = list(map(str, input().split()))
+    if arr[0] == "push":
+        stack.append(int(arr[1]))
+    
+    elif arr[0] == "top":
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
+            
+    elif arr[0] == "size":
+        print(len(stack))
+        
+    elif arr[0] == "empty":
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+            
+    elif arr[0] == "pop":
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
+            stack.pop()
