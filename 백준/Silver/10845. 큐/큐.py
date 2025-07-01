0d06cf78c5d1c5dbfc003c1e0db02b48968024a2
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2025-07-01 14:34:06 +0900

    [Silver IV] Title: 큐, Time: 68 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,45 +1,38 @@
-# push X: 정수 X를 큐에 넣는 연산이다.
-# pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 큐에 정수가 없는 경우에는 -1을 출력한다.
-# size: 큐에 들어있는 정수의 개수를 출력한다.
-# empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
-# front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-# back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-import sys
 from collections import deque
-n = int(sys.stdin.readline().strip())
-queue = []
+import sys
+input = sys.stdin.readline
+
+n = int(input())
 queue = deque()
-for i in range(n):
-    inner = sys.stdin.readline().strip().split()
-    if inner[0] == 'push':
-        queue.append(inner[1])
-    else:
-        if inner[0] == 'pop':
-            if queue: #비어있지 않으면
-                print(queue[0])
-                queue.popleft() #popleft = 맨 앞에 수를 pop함. pop()은 맨 뒤에 수.
-            else:
-                print(-1)
 
-        elif inner[0] == 'size':
-            print(len(queue))
-        
-        elif inner[0] == 'empty':
-            if queue:
-                print(0)
-            else:
-                print(1)
-        
-        elif inner[0] == 'front':
-            if queue:
-                print(queue[0])
-            else:
-                print(-1)
-
+for _ in range(n):
+    arr = list(map(str, input().split()))
+    
+    if arr[0] == "push":
+        queue.append(int(arr[1]))
+    
+    elif arr[0] == "pop":
+        if len(queue) == 0:
+            print(-1)
         else:
-            if queue:
-                print(queue[-1])
-            else:
-                print(-1)
-                
-
+            print(queue.popleft())
+            
+    elif arr[0] == 'size':
+        print(len(queue))
+        
+    elif arr[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+        
+    elif arr[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif arr[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
