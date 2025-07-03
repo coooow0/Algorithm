61344b8c73d57a23a6e78ca1b9f55a5d5f29ddda
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2025-07-03 13:36:37 +0900

    [Silver I] Title: 미로 탐색, Time: 60 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,22 +1,35 @@
 from collections import deque
-n, m = map(int, input().split())
-# 크기를 입력받음
+import sys
+input = sys.stdin.readline
 
-arr = []
-for _ in range(n):
-    arr.append(list(map(int, input())))
-
-queue = deque()
-queue.append([0, 0])
+d = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 def bfs():
     while queue:
         row, col = queue.popleft()
-        for xr, xc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-            new_r = row + xr
-            new_c = col + xc 
-            if 0<= new_r <n and 0<= new_c < m and arr[new_r][new_c] == 1:
-                arr[new_r][new_c] = arr[row][col] + 1
-                queue.append([new_r, new_c])
+        # [0, 0]
+        for nr, nc in d:
+            new_r = row+nr
+            new_c = col+nc
+            
+            if 0 <= new_r < n and 0<= new_c <m:
+                if graph[new_r][new_c] == 1 and not visited[new_r][new_c]:
+                    # 통행할 수 있고, 방문하지 않았을 경우
+                    graph[new_r][new_c] = graph[row][col] + 1
+                    visited[new_r][new_c] = True
+                    queue.append((new_r, new_c))
+    print(graph[n-1][m-1])
+n, m = map(int, input().strip().split())
+graph = []
+
+
+for _ in range(n):
+    graph.append(list(map(int,input().strip())))
+    
+visited = [[False for _ in range(m)] for _ in range(n)]
+
+queue = deque()
+queue.append((0, 0))
+visited[0][0] = True
+# 맨 처음 (1, 1)은 무조건 1인셈.. 큐에 넣어주기
 bfs()
-print(arr[n-1][m-1])
