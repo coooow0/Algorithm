b47e8d801258d09be2b41485004f41d3031d706e
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2025-08-25 23:37:52 +0900

    [Silver V] Title: 돌 게임 2, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/9656. 돌 게임 2/돌 게임 2.py
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.py
@@ -1,14 +1,6 @@
+import sys
+input = sys.stdin.readline
 
 n = int(input())
 
-if n < 3:
-    if n == 1:
-        print('CY')
-    else:
-        print('SK')
-else:
-    if n % 2 == 0:
-        print('SK')
-    else:
-        print('CY')
-        
+print('SK' if n % 2 == 0 else 'CY')
