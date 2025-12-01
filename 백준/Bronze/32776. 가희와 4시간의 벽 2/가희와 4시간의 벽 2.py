366991aaa8ae2897ff8e82342d053c246853de0b
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2025-12-01 18:32:20 +0900

    [Bronze IV] Title: 가희와 4시간의 벽 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/32776. 가희와 4시간의 벽 2/가희와 4시간의 벽 2.py
+++ 백준/Bronze/32776. 가희와 4시간의 벽 2/가희와 4시간의 벽 2.py
@@ -1,13 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-# 4시간 이하일 경우 고속철도가 더 나음
-# 역 a -> 역 b로 갈 때 어떤게 더 나은지,... 
 sab = int(input())
+ma, fab, mb = map(int, input().split())
 
-ma, fab, mb = map(int, input().strip().split())
-
-if sab <= 240 or sab <= ma + fab + mb:
+# 4시간 이하 구간의 경우 고속철도가 더 우위임. 아니면 항공이고
+# 
+if sab <= 240 or sab <= ma + fab +mb:
     print('high speed rail')
 else:
     print('flight')
