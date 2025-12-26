7c624848a0cfa621e1061d14d9d36909766d781d
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2025-12-26 16:46:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 52.13 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,10 +1,17 @@
 def solution(participant, completion):
-    # 마라톤에 참여한 선수들의 이름이 담긴 배열 participant 
-    # 완주한 선수들이 있는 배열 completion
     answer = ""
-    participant.sort()
-    completion.sort()
-    for i, j in zip(participant, completion):
-        if i != j:
-            return i
-    return participant[-1]
+    sum = {}
+    for i in participant:
+        if i in sum:
+            sum[i] += 1
+        else:
+            sum[i] = 1
+        
+    for i in  completion:
+        if i in sum:
+            sum[i] -= 1
+    
+    for i in sum:
+        if sum[i] != 0:
+            answer += i
+    return answer
