ed8a25c07bca661114bf44de80c45be607723621
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2025-12-29 01:22:49 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,11 +1,20 @@
-n = int(input())
-arr=[[0] * 3] * n
+import sys
+input = sys.stdin.readline 
 
-for i in range(n):
-    arr[i] = list(map(int, input().split()))
+n = int(input())
+
+graph = []
+
+for _ in range(n):
+    graph.append(list(map(int, input().strip().split())))
+    
+dp = [[1001] * 3 for _ in range(n)]
+
+dp[0][0], dp[0][1], dp[0][2] = map(int, graph[0])
 
 for i in range(1, n):
-    arr[i][0] = min(arr[i-1][1], arr[i-1][2]) + arr[i][0]
-    arr[i][1] = min(arr[i-1][0], arr[i-1][2]) + arr[i][1]
-    arr[i][2] = min(arr[i-1][1], arr[i-1][0]) + arr[i][2]
-print(min(arr[n-1][0], arr[n-1][1], arr[n-1][2]))
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + graph[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + graph[i][1]
+    dp[i][2] = min(dp[i-1][1], dp[i-1][0]) + graph[i][2]
+    
+print(min(dp[n-1][0], dp[n-1][1], dp[n-1][2]))
