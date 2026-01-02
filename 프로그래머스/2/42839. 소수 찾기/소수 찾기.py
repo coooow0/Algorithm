81c976a17adf414e1cb6e402f0976a809e0c289f
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2026-01-03 01:04:05 +0900

    [level 2] Title: 소수 찾기, Time: 20.33 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,37 +1,27 @@
-from itertools import permutations
+from itertools import permutations 
 import math
+
+def is_prime(n):
+    if n < 2:
+        return False
+    
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    
+    return True
+
 
 def solution(numbers):
     numbers = list(numbers)
-    numbers.sort(reverse=True)
-    num = set()
-    for i in numbers:
-        num.add(int(i))
-    
-    big = 0
-    
-    # 조합이랑 큰 수 찾기
-    for i in range(1, len(numbers) + 1):
-        for block in permutations(numbers, i):
-            block = list(block)
-            block = int(''.join(block))
-            num.add(block)
-            big = max(block, big)
-        
-    answer = [True for _ in range(big+1)]
-    
-    for i in range(2, int(math.sqrt(big)) + 1):
-        if answer[i]:
-            j = 2
-            while i * j <= big:
-                answer[i * j] = False
-                j += 1
     
     cnt = 0
-    
-    for i in range(2, big+1):
-        if answer[i] and i in num:
-            cnt +=1
-        
+    arr = set()
+    for i in range(1, len(numbers) + 1):
+        for num in permutations(numbers, i):
+            num = int(''.join(list(num)))
+            if is_prime(num) and num not in arr:
+                arr.add(num)
+                cnt += 1
+    answer = 0
     return cnt
-
