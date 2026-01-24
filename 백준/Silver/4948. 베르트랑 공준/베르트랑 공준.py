b227ab2beb476c0057831108d2f08623dc4e8677
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2026-01-25 01:03:32 +0900

    [Silver II] Title: 베르트랑 공준, Time: 616 ms, Memory: 34416 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,29 +1,50 @@
 import sys
 input = sys.stdin.readline
 
+# while True:
+#     n = int(input().strip())
+    
+#     if n == 0:
+#         break
+    
+#     graph = [True for _ in range(2 * n + 1)]
+    
+#     for i in range(2, int((2 * n) ** 0.5) + 1):
+#         if graph[i]:
+#             j = 2
+#             while i * j <= 2*n:
+#                 graph[i*j] = False
+#                 j += 1
+    
+#     cnt = 0
+#     if n == 1:
+#         print(1)
+#         continue
+#     else:
+#         for i in range(n + 1, 2 * n + 1):
+#             if graph[i]:
+#                 cnt += 1
+            
+#     print(cnt)
+    
+max_num = 123456 * 2
+graph = [True for _ in range(max_num + 1)]
+
+for i in range(2, int(max_num ** 0.5) + 1):
+    if graph[i]:
+        j = 2
+        while i * j <= max_num:
+            graph[i * j] = False
+            j += 1
+
 while True:
     n = int(input().strip())
     
     if n == 0:
         break
     
-    graph = [True for _ in range(2 * n + 1)]
-    
-    for i in range(2, int((2 * n) ** 0.5) + 1):
-        if graph[i]:
-            j = 2
-            while i * j <= 2*n:
-                graph[i*j] = False
-                j += 1
-    
     cnt = 0
-    if n == 1:
-        print(1)
-        continue
-    else:
-        for i in range(n + 1, 2 * n + 1):
-            if graph[i]:
-                cnt += 1
-            
+    for i in range(n + 1, 2 * n + 1):
+        if graph[i]:
+            cnt += 1
     print(cnt)
-    
