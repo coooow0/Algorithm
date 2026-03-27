dec71f0dcd83a1da0cb35b1e8a741819585d41f6
Soyoung Yun <105861622+coooow0@users.noreply.github.com>
2026-03-27 10:54:38 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2568 ms, Memory: 126424 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,13 +1,21 @@
+# 수 정렬하기
 import sys
 input = sys.stdin.readline
+
 n = int(input())
-arr = [0] * (10000+1) # 해당 수는 10000보다 작거나 같으니까
+# 메모리 제한이 8MB로 무지 적은 편. 메모리를 잘 써야한다... 
+# arr[입력한 수] = 입력받은 횟수(개수)로 놓고 개수가 1개 이상이면 그만큼 출력하도록 함 
+# -> 10000개의 배열을 쓸 수 있음. 아니면 10,000,000개의 배열을 써야할 수도 있음.
+# 그에 반해 시간은 널널해서 for문 많이 돌려도 상관업슬듯..
 
+arr = [0] *(10000 + 1)
 for _ in range(n):
-    i = int(input())
-    arr[i] += 1 # 입력받은 숫자의 배열 값이 증가함
-
+    m = int(input())
+    arr[m] += 1
+    
 for i in range(len(arr)):
-    if arr[i] != 0: # 입력을 받은 배열이라면
-        for _ in range(arr[i]): # 배열의 값 만큼 출력 
+    if arr[i] != 0: # 입력받은 적이 있을 경우
+        for _ in range(arr[i]):
             print(i)
+    
+      
